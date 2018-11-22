f77e7034cc46339c492010e92cece7b387f90782
KeatonH123 <43858357+KeatonH123@users.noreply.github.com>
2018-11-22 15:23:13 -0700

    Add files via upload
    This code includes the creation of two webpages with a link from the second to the first.

---
+++ run.sh
@@ -0,0 +1,28 @@
+cd /var/www/html
+mkdir index A
+cd index
+touch index.html
+echo "<html>
+<head>
+<title>Welcome to my website!</title>
+</head>
+<body>
+Hello, this is my website.
+<a href="A.html">Click here to see my contact info</a>
+</body>
+</html>" > index.html
+cd ..
+cd a/
+touch A.html
+echo "<html>
+Contact info: keatonhanna@yahoo.com
+</html>" > A.html
+
+
+
+
+
+
+
+
+
